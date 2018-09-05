d01a303bdc8cdf8efd36cb268df304a3e4bdbe63
Dana Negrescu <dana@boxalino.com>
2018-09-05 15:44:59 +0200

    switched branch to mooris project

--- docker/ruby/bin/run.sh
+++ docker/ruby/bin/run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-bundle pack
+bundle install
+bundle pack --all
 ruby bin/rails db:setup
 ruby bin/rails log:clear tmp:clear
-ruby bin/yarn
 
-rails server
+bundle install
+
+ruby bin/rails server
 
 exec "$@"
